abac70ef41cd0e91eceb71f311a2c320c8f33dba
root <root@DESKTOP-2VPR9IB>
2024-03-17 13:40:04 -0300

    ✔ Desafio FlashCards Concluido

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,62 @@
+import { useState } from "react"
+
 export function App() {
-  return <h1>Boilerplate Vite + React</h1>;
+  const [showCardID, setShowCardID] = useState()
+  
+  const cardsInfo = [
+    {
+      id: 7336,
+      question: 'Do quê aplicações React são feitas?',
+      answer: 'Componentes',
+    },
+    {
+      id: 8832,
+      question: 'Qual é o nome da sintaxe usada para descrever UI no React?',
+      answer: 'JSX',
+    },
+    {
+      id: 3457,
+      question: 'Em qual linguagem o React é baseado?',
+      answer: 'JavaScript',
+    },
+    {
+      id: 9103,
+      question: 'Qual método usar para renderizar uma lista no React?',
+      answer: 'map',
+    },
+    {
+      id: 1297,
+      question: 'Qual empresa criou o React?',
+      answer: 'Meta',
+    },
+    {
+      id: 2002,
+      question: 'Como passar informações para um componente React?',
+      answer: 'Através de props',
+    },
+  ]
+
+  function handleCardClick(id) {
+    if(showCardID === id) {
+      return setShowCardID(0)
+    }
+
+    setShowCardID(id)
+  }
+
+  return (
+    <div className="flashcards">
+      {cardsInfo.map((card) => {
+        return (
+          <div
+            key={card.id}
+            className="card"
+            onClick={() => handleCardClick(card.id)}
+          >
+            {showCardID === card.id ? card.answer : card.question}
+          </div>
+        )
+      })}
+    </div>
+  )
 }
